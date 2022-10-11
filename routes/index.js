10e53555e1c05672e2cdfd3e3e072d6e87c13cb6
hhhoua <527699871@qq.com>
2022-10-11 14:08:24 +0800

    空行

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ router.get('/',function(req,res){
     // 路由是相对路径，相对于模板根目录（views）,不要写./直接写/代表根目录，扩展名.html可以不写
     res.render('index',{title:'首页'})
 })
+
 router.all('/secret', function (req, res, next) {
     console.log('Accessing the secret section ...')
     next() // pass control to the next handler
